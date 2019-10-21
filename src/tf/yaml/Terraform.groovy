03740ff8c9551d06177e0743bb5c036cb2db13b7
Scott Miller <fsm5yd@virginia.edu>
2019-10-21 14:24:09 -0400

    typo

--- src/tf/yaml/Terraform.groovy
+++ src/tf/yaml/Terraform.groovy
@@ -46,7 +46,7 @@ class Terraform implements Serializable {
 
     public static ArrayList getResourceParameters(resourceGroup, type) {
         def parameters = []
-        for (resource in resourceGrousp[resourceGroup[resourceGroup]]['resources']) {
+        for (resource in resourceGroups[resourceGroup[resourceGroup]]['resources']) {
             if (resource['type'] == type) {
                 parameters += new StringParameterDefinition(resource.key, "")
             }
